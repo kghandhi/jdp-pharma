a3920cef930e4f75ea5997609501103fbaf6317a
kghandhi <kghandhi@uchicago.edu>
2019-06-21 09:27:22 -0700

    adding rest

---
+++ JDP_Pharma_Consulting,_LLC/Product_and_Process_Development_files/Product_and_Process_Development.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20190616
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(2.8284,2.8284),color:'#41489e',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Product_and_Process_Development_files/Product_and_Process_DevelopmentMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
---
+++ JDP_Pharma_Consulting,_LLC/Welcome_to_JDP_Pharma_files/Welcome_to_JDP_Pharma.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20190616
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(2.8284,2.8284),color:'#41489e',opacity:0.750000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,139),url:'Welcome_to_JDP_Pharma_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Welcome_to_JDP_Pharma_files/stroke_1.png'},{rect:new IWRect(1,-1,699,2),url:'Welcome_to_JDP_Pharma_files/stroke_2.png'},{rect:new IWRect(700,-1,2,2),url:'Welcome_to_JDP_Pharma_files/stroke_3.png'},{rect:new IWRect(700,1,2,139),url:'Welcome_to_JDP_Pharma_files/stroke_4.png'},{rect:new IWRect(700,140,2,2),url:'Welcome_to_JDP_Pharma_files/stroke_5.png'},{rect:new IWRect(1,140,699,2),url:'Welcome_to_JDP_Pharma_files/stroke_6.png'},{rect:new IWRect(-1,140,2,2),url:'Welcome_to_JDP_Pharma_files/stroke_7.png'}],new IWSize(701,141))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_to_JDP_Pharma_files/Welcome_to_JDP_PharmaMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
